260a2518fe1490960ddb714759adaa4211975612
Ali <120646614+alizamanian79@users.noreply.github.com>
2024-12-03 09:47:30 +0330

    JwtAuthenticationFilter completed

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,7 +19,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    final private JwtService jwtService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -39,6 +43,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     jwt=authHeader.substring(7); // Start Reading after 7 character ( Refer ti Bearer )
 
     userEmail= jwtService.extractUserName(jwt); // to Extract
+
+    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+    }
+
 
 
     }
